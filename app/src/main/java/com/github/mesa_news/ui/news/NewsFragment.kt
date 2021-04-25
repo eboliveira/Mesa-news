cd3ea73accef1571698396b76260738f97732e87
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2021-04-25 15:07:21 -0300

    refactor(news): refactor fragment to use mvvm architecture with databinding

--- app/src/main/java/com/github/mesa_news/ui/news/NewsFragment.kt
+++ app/src/main/java/com/github/mesa_news/ui/news/NewsFragment.kt
@@ -4,20 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.github.mesa_news.R
+import com.github.mesa_news.databinding.FragmentNewsBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class NewsFragment : Fragment() {
+    private lateinit var newsViewModel: NewsViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
-    }
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding: FragmentNewsBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_news,
+            container,
+            false
+        )
 
+        binding.lifecycleOwner = viewLifecycleOwner
+        newsViewModel = ViewModelProvider(this).get(NewsViewModel::class.java)
+        binding.viewModel = newsViewModel
+        return binding.root
+    }
 }
---
+++ app/src/main/java/com/github/mesa_news/ui/news/NewsViewModel.kt
@@ -0,0 +1,6 @@
+package com.github.mesa_news.ui.news
+
+import androidx.lifecycle.ViewModel
+
+class NewsViewModel: ViewModel() {
+}
