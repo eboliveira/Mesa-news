87acb5575ae1e05d5ac989b4d94589b760f2d3d0
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2021-04-25 16:32:30 -0300

    fix(api): fix created http client not being used

--- app/src/main/java/com/github/mesa_news/data/remote/NewsInterface.kt
+++ app/src/main/java/com/github/mesa_news/data/remote/NewsInterface.kt
@@ -4,9 +4,9 @@ import com.github.mesa_news.data.models.PagedNews
 import retrofit2.http.GET
 
 interface NewsInterface {
-    @GET("/client/news")
+    @GET("client/news")
     suspend fun news(): PagedNews
 
-    @GET("/client/news/hightlights")
+    @GET("client/news/highlights")
     suspend fun highlights(): PagedNews
 }
